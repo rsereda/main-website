62c08403ff7fc94c9f253cf568ef43ef1780837c
Christoph <staudtc@live.de>
2016-09-19 16:51:51 +0200

    Update Plugins

--- plugins/rainlab/pages/vendor/autoload.php
+++ plugins/rainlab/pages/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitcb2fbbe345333f6227e546c155a74123::getLoader();
+return ComposerAutoloaderInitdbc057be141d9e8e825221bdafc498f7::getLoader();
